dac0247cac11a66094d7e5c8314459d2e5ab7878
Jean M Tessier <jean.tessier93@gmail.com>
2026-06-20 16:12:23 -0400

    fix(spawn): remove unused os import (Task 28 follow-up)

--- packages/pi-delegate/src/parent/spawn.ts
+++ packages/pi-delegate/src/parent/spawn.ts
@@ -4,7 +4,6 @@
  */
 
 import * as fs from 'fs/promises';
-import * as os from 'os';
 import * as path from 'path';
 import { spawn } from 'child_process';
 import type { ChildProcess } from 'child_process';
